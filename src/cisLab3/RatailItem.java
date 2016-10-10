761073c2e940b3ae36ce46e59e2b9c1a9ab49a4e
joseocasio <joseocasio@192.168.148.78>
2016-10-10 19:00:57 -0400

    final commit

--- src/cisLab3/RatailItem.java
+++ src/cisLab3/RatailItem.java
@@ -6,26 +6,39 @@ public class RatailItem
  private int unitsOnHand; 
  private double price;
  
- 
+
+ public void setDescription(String description) {
+		this.description = description;
+	}
 public String getDescription() {
 	return description;
 }
-public void setDescription(String description) {
-	this.description = description;
-}
 
+public void setUnitsOnHand(int unitsOnHand) {
+	this.unitsOnHand = unitsOnHand;
+	if (unitsOnHand<0)
+	{
+		throw new IllegalArgumentException();
+	}
+}
 public int getUnitsOnHand() {
 	return unitsOnHand;
 }
-public void setUnitsOnHand(int unitsOnHand) {
-	this.unitsOnHand = unitsOnHand;
-}
 
+public void setPrice(double price) {
+	this.price = price;
+	if (price<0)
+	{
+		throw new IllegalArgumentException();
+	}
+} 
 public double getPrice() {
 	return price;
 }
-public void setPrice(double price) {
-	this.price = price;
-} 
- 
+
+public String toString()
+{
+	return String.format("Item:%2s%10s%12s", description, unitsOnHand, (double)price );
+}
+
 }
--- src/cisLab3/RetailItemMain.java
+++ src/cisLab3/RetailItemMain.java
@@ -5,56 +5,68 @@ import java.util.Scanner;
 public class RetailItemMain {
 
 	public static void main(String[] args) {
-		Scanner keyboard = new Scanner (System.in);
+		Scanner keyboard = new Scanner(System.in);
 		int numItems;
-		int userInput; 
-		String description=""; 
-		int units=0; 
-		double price=0;
-		int items=0;
-		RatailItem r = new RatailItem();
-		
-	try 
-	{
-		
-		while(items < 3){
-			try {
-			
-		System.out.print("Enter description:" );
-		r.setDescription(description=keyboard.nextLine());
-		items++;
-		System.out.print("Enter units on hand:" );
-		r.setUnitsOnHand(units=keyboard.nextInt());
-		items++;
-	
-		
-		System.out.print("Enter the price: ");
-		r.setPrice(price=keyboard.nextDouble());
-		items++;
-		
-		
-		if (units<0)
-		{
-			throw new IllegalArgumentException();
-		}
-		if (price<0)
-		{
-			throw new IllegalArgumentException();
-		}
-		
-			}catch(IllegalArgumentException e)
-			 {
+		int userInput;
+		String description = "";
+		int unitsOnHand = 0;
+		double price = 0;
+		int items = 0;
+
+		try {
+
+			RatailItem r = new RatailItem();
+			{
+
+				try {
+					boolean valid = false;
+
+					while (!valid) {
+
+						System.out.print("Enter description:");
+						r.setDescription(description = keyboard.nextLine());
+
+						while (!valid) {
+							try {
+
+								System.out.print("Enter units on hand:");
+								r.setUnitsOnHand(unitsOnHand = keyboard.nextInt());
+								valid = true;
+
+							} catch (Exception e) {
+
+								System.out.println("Invalid number must be positive. ");
+							}
+						}
+						
+							valid=false;
+							
+						while (!valid) {
+							try {
+
+								System.out.print("Enter the price: ");
+								r.setPrice(price = keyboard.nextDouble());
+								valid = true;
+
+							} catch (Exception e) {
+								System.out.println("Invalid number must be positive. ");
+							}
+						}
+
+						items++;
+					}
+				} catch (IllegalArgumentException e) {
 					System.out.println("Negative numbers not allowed. Try again. ");
-					keyboard.next();
-			 }
+					keyboard.nextLine();
+				}
+			}
+			System.out.println("\nDescription \t Units on Hand \t Price");
+			System.out.println("____________________________________________");
+			System.out.println(r);
+
+		} finally {
+			System.exit(0);
+
 		}
-		System.out.println("\nDescription \t Units on Hand \t Price");
-		System.out.println("_______________________________________________");
-		System.out.println("Item:"+description+ "\t\t"+ units+ "\t  $"+ price);
-		
-	} finally{
-		System.exit(0);
-		
-	}
 	}
 }
